6c191eefc70990dcbc28768eaab450271b265563
adr <rodrigues.adriano2607@gmail.com>
2025-01-08 01:59:26 -0300

    Remove Pokémon icon SVG and add broken link image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
-'use client';
+"use client";
 
-import { Pokemon } from '@/type/type';
-import getData from './../http/http';
+import { Pokemon } from "@/type/type";
+import getData from "./../http/http";
 import { useEffect, useState } from "react";
-import Image from 'next/image';
-import getColorByType from './../util/ColorByType';
+import Image from "next/image";
+import getColorByType from "./../util/ColorByType";
 import { Sigmar_One } from "next/font/google";
-import { Nunito } from 'next/font/google';
+import { Nunito } from "next/font/google";
 
 const roboto = Sigmar_One({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
-const nunito = Nunito({ subsets: ['latin'] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export default function Home() {
-  const url = 'https://pokeapi.co/api/v2/pokemon?limit=16&offset=0';
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=16&offset=0";
   const [pokedata, setPokeData] = useState<Pokemon[]>([]);
   const [descriptions, setDescriptions] = useState<Record<number, string>>({}); // Estado para descrições
+  const [types, setTypesDamage] = useState<
+    Record<number, { fraquezas: string; resistencias: string } | string>
+  >({}); // Estado para fraquezas e resistências
 
   useEffect(() => {
     fetchPokemonData();
@@ -41,6 +44,7 @@ export default function Home() {
 
         // Busca descrições de todos os Pokémon
         fetchAllDescriptions(detailedPokemonData);
+        fetchTypes(detailedPokemonData);
       }
     } catch (error) {
       console.error("Erro ao buscar dados dos Pokémon's:", error);
@@ -48,30 +52,87 @@ export default function Home() {
   };
 
   // Busca as descrições para todos os Pokémon
-const fetchAllDescriptions = async (pokemonData: Pokemon[]) => {
-  const descriptionsArray: string[] = []; // Usando um array para armazenar as descrições
+  const fetchAllDescriptions = async (pokemonData: Pokemon[]) => {
+    const descriptionsArray: string[] = []; // Usando um array para armazenar as descrições
 
-  await Promise.all(
-    pokemonData.map(async (pokemon) => {
-      try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`);
-        const speciesData = await response.json();
-        const description = speciesData.flavor_text_entries.find((entry: { language: { name: string }; flavor_text: string }) => entry.language.name === 'en')?.flavor_text || 'Descrição não disponível.';
-        descriptionsArray[pokemon.id] = description; // Armazena a descrição no índice correspondente ao ID
-      } catch (error) {
-        console.error(`Erro ao buscar descrição para Pokémon ${pokemon.name}: ${(error as Error).message}`);
-        descriptionsArray[pokemon.id] = 'Erro ao buscar descrição.'; // Insere mensagem de erro no índice
-      }
-    })
-  );
+    await Promise.all(
+      pokemonData.map(async (pokemon) => {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`
+          );
+          const speciesData = await response.json();
+          const description =
+            speciesData.flavor_text_entries.find(
+              (entry: { language: { name: string }; flavor_text: string }) =>
+                entry.language.name === "en"
+            )?.flavor_text || "Descrição não disponível.";
+          descriptionsArray[pokemon.id] = description; // Armazena a descrição no índice correspondente ao ID
+        } catch (error) {
+          console.error(
+            `Erro ao buscar descrição para Pokémon ${pokemon.name}: ${
+              (error as Error).message
+            }`
+          );
+          descriptionsArray[pokemon.id] = "Erro ao buscar descrição."; // Insere mensagem de erro no índice
+        }
+      })
+    );
 
-  setDescriptions(descriptionsArray); // Atualiza o estado com o array de descrições
-};
+    setDescriptions(descriptionsArray); // Atualiza o estado com o array de descrições
+  };
 
+  // Busca fraquezas e resistências para cada Pokémon
+  const fetchTypes = async (pokemonData: Pokemon[]) => {
+    const typesData: Record<
+      number,
+      { fraquezas: string; resistencias: string }
+    > = {};
+
+    await Promise.all(
+      pokemonData.map(async (pokemon) => {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/type/${pokemon.types[0].type.name}/`
+          );
+          const speciesData = await response.json();
+
+          const double_damage_from =
+            speciesData.damage_relations.double_damage_from.find(
+              (entry) => entry.name
+            )?.name || "Nenhuma";
+
+          const half_damage_from =
+            speciesData.damage_relations.half_damage_from.find(
+              (entry) => entry.name
+            )?.name || "Nenhuma";
+
+          typesData[pokemon.id] = {
+            fraquezas: double_damage_from,
+            resistencias: half_damage_from,
+          };
+        } catch (error) {
+          console.error(
+            `Erro ao buscar fraquezas e resistências para Pokémon ${
+              pokemon.name
+            }: ${(error as Error).message}`
+          );
+          typesData[pokemon.id] = {
+            fraquezas: "Erro",
+            resistencias: "Erro",
+          };
+        }
+      })
+    );
+
+    setTypesDamage(typesData); // Atualiza o estado com o tipo de dano
+  };
 
   return (
     <>
-      <header className={`${roboto.className} border-b border-gray-800 shadow-sm`}>
+      <header
+        className={`${roboto.className} border-b border-gray-800 shadow-sm`}
+      >
         <div className="mx-auto container py-4">
           <h1 className="text-2xl text-yellow-400">POKEMON</h1>
         </div>
@@ -83,37 +144,97 @@ const fetchAllDescriptions = async (pokemonData: Pokemon[]) => {
               key={index}
               className="shadow-xl rounded-lg cursor-pointer hover:scale-105 transition-all duration-300 hover:bg-[#1f1f3d]"
             >
-              <div className={`text-center border border-gray-700 rounded relative h-full }`}>
+              <div
+                className={`text-center border border-gray-700 rounded relative h-full }`}
+              >
                 <div
-                  style={{ backgroundColor: getColorByType(item.types[0].type.name) }}
-                  className={`flex justify-between items-center px-2 text-${item.types[0].type.name === 'normal' ? 'black' : 'white'}`}
+                  style={{
+                    backgroundColor: getColorByType(item.types[0].type.name),
+                  }}
+                  className={`flex justify-between items-center px-2 text-${
+                    item.types[0].type.name === "normal" ? "black" : "white"
+                  }`}
                 >
-                  <h2 className={`capitalize font-bold text-lg `}>{item.name}</h2>
+                  <h2 className={`capitalize font-bold text-lg `}>
+                    {item.name}
+                  </h2>
                   <div className="font-extrabold text-inherit">
-                    <span className="text-xs italic">hp{' '}</span>
+                    <span className="text-xs italic">hp </span>
                     <span className="text-lg italic">
-                      {item.stats.find((cc) => cc.stat.name === 'hp')?.base_stat ?? 'N/A'}
+                      {item.stats.find((cc) => cc.stat.name === "hp")
+                        ?.base_stat ?? "N/A"}
                     </span>
                   </div>
                 </div>
                 <Image
                   className="mx-auto"
-                  src={item.sprites?.other?.['official-artwork'].front_default || '/path/to/default/image.png'}
+                  src={
+                    item.sprites?.other?.["official-artwork"].front_default ||
+                    "/path/to/default/image.png"
+                  }
                   width={250}
                   height={250}
                   alt={`Imagem do Pokémon ${item.name}`}
                 />
-                                  <span className='text-sm text-white font-extrabold italic absolute right-4 bottom-2'>#{item.id}</span>
+                <span className="text-sm text-white font-extrabold italic absolute right-4 bottom-2">
+                  #{item.id}
+                </span>
 
                 <div className="bg-[#242233] min-h-52 text-white  flex-grow flex flex-col">
                   {/* Exibe a descrição do Pokémon */}
-                  <span className='flex-grow break-words px-2 pt-2 font-semibold'>{descriptions[item.id] || 'Carregando descrição...'}</span>
-                
-                  <span className=" ">fraqueza</span>
-                  <span className=" ">poder</span>
-                  <span className=" ">características</span>
-                </div>  
-               
+                  <span className="flex-grow break-words px-2 pt-2 font-semibold">
+                    {descriptions[item.id] || "Carregando descrição..."}
+                  </span>
+
+                  <ul className="flex gap-8 font-semibold justify-center items-center pb-4  ">
+                    <li
+                      style={{
+                       
+                        borderRightWidth:2,
+                        borderRightColor:
+                          types[item.id] &&
+                          getColorByType(types[item.id].fraquezas),
+                      }}
+                     className=" flex gap-1 capitalize pr-2"
+                    >
+                      <Image
+                        src="/broken-link.png"
+                        width={25}
+                        height={25}
+                        alt="a"
+                      />
+
+                      {types[item.id]
+                        ? types[item.id].fraquezas
+                        : "Carregando fraquezas..."}
+                    </li>
+
+                    <li
+                      style={{
+                        borderRightWidth:2,
+                        borderRightColor:
+                          types[item.id] &&
+                          getColorByType(types[item.id].resistencias),
+                      }}
+                      className=" flex gap-1 capitalize pr-2"
+                    >
+                      <Image
+                        src="/antibiotic-resistant.png"
+                        width={25}
+                        height={25}
+                        alt="a"
+                      />
+                      {types[item.id]
+                        ? types[item.id].resistencias
+                        : "Carregando resistências..."}
+                    </li>
+                  </ul>
+
+                  {/* Exibe fraquezas e resistências
+                  <span className='text-sm text-white'>
+                    // {types[item.id] ? `Fraquezas: ${types[item.id].fraquezas}, Resistências: ${types[item.id].resistencias}` : 'Carregando fraquezas e resistências...'}
+                  </span> */}
+                </div>
               </div>
             </div>
           ))}
